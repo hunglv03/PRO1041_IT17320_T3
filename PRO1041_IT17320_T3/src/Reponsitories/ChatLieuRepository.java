4ee363442b607cb79f0533463bef9b5a66cbc4d7
hunglvph25428 <118031034+hunglvph25428@users.noreply.github.com>
2022-11-19 22:11:31 +0700

    ChatLieu

---
+++ PRO1041_IT17320_T3/src/Reponsitories/ChatLieuRepository.java
@@ -0,0 +1,85 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Reponsitories;
+
+
+import Util.DBContext;
+import ViewModels.ChatLieuViewModel;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+
+/**
+ *
+ * @author Lvh9x
+ */
+public class ChatLieuRepository {
+
+    final String INSERT = "INSERT INTO CHATLIEU(Ma,Ten) VALUES(?,?)";
+    final String UPDATE = "UPDATE CHATLIEU SET Ma = ?, Ten = ? WHERE Id = ?";
+    final String DELETE = "DELETE FROM CHATLIEU WHERE Id = ?";
+
+    public ChatLieuRepository() {
+    }
+
+    public List<ChatLieuViewModel> GetAll() {
+        String SELECT_CHATLIEU = "SELECT * FROM CHATLIEU";
+        List<ChatLieuViewModel> _lstChatLieu = new ArrayList<>();
+        try ( Connection conn = DBContext.getConnection()) {
+            Statement st = conn.createStatement();
+            ResultSet rs = st.executeQuery(SELECT_CHATLIEU);
+            while (rs.next()) {
+                _lstChatLieu.add(new ChatLieuViewModel(rs.getString(1), rs.getString(2), rs.getString(3)));
+            }
+        } catch (Exception e) {
+            System.out.println("Không thể kết nối lại GetAll");
+        }
+        return _lstChatLieu;
+    }
+
+    //add
+    public boolean Add(ChatLieuViewModel cl) {
+        try ( Connection conn = DBContext.getConnection()) {
+            PreparedStatement ps = conn.prepareStatement(INSERT);
+            ps.setString(1, cl.getMa());
+            ps.setString(2, cl.getTen());
+            ps.execute();
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("Lỗi tại AddRepo");
+            return false;
+        }
+    }
+
+    public boolean Update(ChatLieuViewModel cl) {
+        try ( Connection conn = DBContext.getConnection()) {
+            PreparedStatement ps = conn.prepareStatement(UPDATE);
+            ps.setString(1, cl.getMa());
+            ps.setString(2, cl.getTen());
+            ps.setString(3, cl.getId());
+            ps.execute();
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("Lỗi tại UpdateRepo");
+            return false;
+        }
+    }
+
+    public boolean Delete(ChatLieuViewModel cl) {
+        try ( Connection conn = DBContext.getConnection()) {
+            PreparedStatement ps = conn.prepareStatement(DELETE);
+            ps.setString(1, cl.getId());
+            ps.execute();
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("Lỗi tại DeleteRepo");
+            return false;
+        }
+    }
+}
