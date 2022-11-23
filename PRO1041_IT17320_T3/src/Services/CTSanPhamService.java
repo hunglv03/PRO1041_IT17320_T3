194d6b3bb58da830d8b984e0bea24add302814cb
hunglvph25428 <Lvh9x8z5102003@gmail.com>
2022-11-23 11:06:04 +0700

    CTSanPham

---
+++ PRO1041_IT17320_T3/src/Services/CTSanPhamService.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package Services;
+
+import ViewModels.CTSanPhamViewModel;
+import java.util.List;
+
+/**
+ *
+ * @author Lvh9x
+ */
+public interface CTSanPhamService {
+
+    public String them(CTSanPhamViewModel ct);
+
+    public String sua(CTSanPhamViewModel ct);
+
+    public String xoa(CTSanPhamViewModel ct);
+
+    public List<CTSanPhamViewModel> GetAll();
+    
+}
