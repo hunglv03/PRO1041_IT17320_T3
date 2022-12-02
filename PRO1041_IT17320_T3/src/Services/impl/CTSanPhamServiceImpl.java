c5ccdc86a82f02bf4b8dea3854d66724a3ee9bac
hunglvph25428 <Lvh9x8z5102003@gmail.com>
2022-12-02 22:11:29 +0700

    Done

--- PRO1041_IT17320_T3/src/Services/impl/CTSanPhamServiceImpl.java
+++ PRO1041_IT17320_T3/src/Services/impl/CTSanPhamServiceImpl.java
@@ -9,6 +9,7 @@ import Reponsitories.CTSanPhamRepository;
 import Reponsitories.ChatLieuRepository;
 import Services.CTSanPhamService;
 import ViewModels.CTSanPhamViewModel;
+import ViewModels.SizeVM;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -55,6 +56,20 @@ public class CTSanPhamServiceImpl implements CTSanPhamService {
         ctsp.GetAll().forEach(x->{
             System.out.println(x);
         });
+    }
+
+    @Override
+    public List<CTSanPhamViewModel> getAll(String input) {
+        if (input == null) {
+            return repoCTSP.GetAll();
+        }
+        List<CTSanPhamViewModel> tempctsp = new ArrayList<>();
+        for (CTSanPhamViewModel x : repoCTSP.GetAll()) {
+            if (x.getIdSp().getTen().toLowerCase().contains(input.toLowerCase()) || x.getIdSize().getTen().toLowerCase().contains(input.toLowerCase()) || x.getIdMauSac().getTen().toLowerCase().contains(input.toLowerCase()) || x.getIdChatLieu().getTen().toLowerCase().contains(input.toLowerCase())) {
+                tempctsp.add(x);
+            }
+        }
+        return tempctsp;
     }
 }
 
